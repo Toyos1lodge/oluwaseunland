a2c0aab524be6d72034a711dfcedbf9080423f3b
Toyos1lodge <132411741+Toyos1lodge@users.noreply.github.com>
2023-05-03 01:00:44 +0100

    Add files via upload

---
+++ prototype.js
@@ -0,0 +1,22 @@
+ScrollReveal().reveal('.navbar', {
+     delay: 500
+});
+ScrollReveal().reveal('.carousel', { 
+    delay: 900
+});
+ScrollReveal().reveal('', { 
+    delay: 1100
+});
+ScrollReveal().reveal('.parag', { 
+    delay: 1300
+});
+
+ScrollReveal().reveal('.card', {
+    interval: 18, 
+    reset: true
+});
+
+ScrollReveal(). reveal('.section-box', {
+    interval: 18,
+    reset: true
+});
